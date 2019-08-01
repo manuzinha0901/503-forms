c13c5e672dfe08f89841245f108d1146f16da139
manuzinha0901 <emanuelle.sanntoslima@gmail.com>
2019-08-01 14:56:18 -0300

    atividade 503-forms

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
-let nome = document.querySelector("nome");
-let email = document.querySelector("email");
-let email = document. querySelector("email");
-let password =document. querySelector("senha");
+let nome = document.querySelector("#nome");
+let email = document.querySelector("#email");
+let confirmaremail = document.querySelector("#confirmaremail");
+let password = document.querySelector("#senha");
 
-if(localStorage.nome){
-Div.style.display = "none";
-TextDecoder.innerHTML=`Seja bem Vindo ${localStorage.nome}`;   
-}
-else{
-form.style.display="block";
+console.log(nome);
+
+localStorage.setItem("nome",nome.value);
+localStorage.setItem("email",email.value);
+localStorage.setItem("confirmaremail",email.value);
+localStorage.setItem("senha",senha.value);
+
+  if(localStorage.nome){
+ Div.style.display = "none";
+ TextDecoder.innerHTML=`Seja bem Vindo ${localStorage.nome}`;   
+ }
+ else{
+ form.style.display="block";
 Section.style.display="none";
+
 function cadastrar(){ 
-    localStorage.setItem("nome",caixatexto.value);
-    if(localStorage.nome){
-    form.style.display="none";
-    Section.style.display="block";
-    texto.innerHTML=`seja bem vindo ${localStorage.nome}`;
-    naoso.innerHTML=`trocar ${localStorage.nome}`;
+     localStorage.setItem("nome",caixatexto.value);
+  if(localStorage.nome){
+     form.style.display="none";
+     Section.style.display="block";
+     texto.innerHTML=`seja bem vindo ${localStorage.nome}`;
+     naosou.innerHTML=`trocar ${localStorage.nome}`;
 
      }
-   }
+  }
 }
-function limparstorange(){
-    localStorage.clear();
+ function limparstorange(){
+     localStorage.clear();
 }
